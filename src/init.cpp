d3faf73c74722c3ad71e8874a3cc39f2ebb0ddac
David <aglandawood@gmail.com>
2024-11-07 23:34:48 +0200

    added httplib library & basic server testing

--- src/init.cpp
+++ src/init.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "libraries/httplib.h"
+
+using namespace httplib;
+
+/*
+    Server testing. This is so fucking awesome
+*/
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Running server." << std::endl;
+
+    Server server;
+
+    server.Get("/sigma", [](const Request& /*req*/, Response& res) {
+        std::cout << "Received GET request on /sigma." << std::endl;
+        res.set_content("I hate sigmas.", "text/plain");
+    });
+
+    server.listen("0.0.0.0", 12800);
 }
