ebff4178ed8688d10e18228b7f6268df28c7e8ee
David <aglandawood@gmail.com>
2024-11-08 14:27:33 +0200

    json response

--- src/init.cpp
+++ src/init.cpp
@@ -17,7 +17,9 @@ int main()
 
     server.Get("/sigma", [](const Request& /*req*/, Response& res) {
         std::cout << "Received GET request on /sigma." << std::endl;
-        res.set_content("I hate sigmas.", "text/plain");
+        
+
+        res.set_content("{\"message\": \"I hate sigmas.\"}", "application/json");
     });
 
     server.listen("127.0.0.1", PORT);
