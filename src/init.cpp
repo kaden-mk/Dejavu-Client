1de6c8f90e74dba5882062cd3bd6fce11060e569
Astr0verload <bhopping.rabbit@gmail.com>
2024-11-09 17:07:26 +0300

    boom shakalaka

--- src/init.cpp
+++ src/init.cpp
@@ -6,39 +6,14 @@
 #include "libraries/httplib.h"
 
 using json = nlohmann::json;
-
-// Responses
-#include "responses/catalog.h"
-#include "responses/calendar.h"
-#include "responses/fortnite-game.h"
-#include "responses/DefaultEngine.h"
-#include "responses/DefaultGame.h"
-#include "responses/DefaultRuntimeOptions.h"
-#include "responses/athenaProfile.h"
-#include "responses/common_core.h"
-#include "responses/common_public.h"
-#include "responses/collections.h"
-#include "responses/creative.h"
-
 using namespace httplib;
 
 Server server;
 
-// Modules
-#include "modules/statics.h"
-#include "modules/dynamics.h"
-#include "modules/api.h"
-#include "modules/profile.h"
 #include "modules/commands.h"
 
 int main()
 {
-    const short PORT = 8080;
-
-    initStatics();
-    initDynamics();
-    initApi();
-    initProfile();
     InitCommands();
 
     std::string input;
